0b5cb21f6df6c949116094cf7fc77a8d50164b22
Roshani Kunwar <roshaniKunwar4009@gmail.com>
2025-02-05 17:18:00 +0545

    Initial commit

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -18,7 +18,7 @@ const PlaceOrder = () => {
 		product_service_charge: "0",
 		product_delivery_charge: "0",
 		product_code: "EPAYTEST",
-		success_url: "https://ecommerce-xdezo.vercel.app/paymentSuccess",
+		success_url: "https://rc-epay.esewa.com.np/success",
 		failure_url: "https://ecommerce-xdezo.vercel.app/paymentfailure",
 		signed_field_names: "total_amount,transaction_uuid,product_code",
 		signature: "",
