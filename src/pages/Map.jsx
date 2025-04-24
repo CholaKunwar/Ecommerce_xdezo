173df18252f41ac743fabef7c630bc910344eaba
Roshani Kunwar <roshaniKunwar4009@gmail.com>
2025-04-24 09:41:11 +0545

    update2

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -84,7 +84,7 @@ const Map = () => {
 
 				{/* Contact Form Section */}
 				<motion.div
-					className="w-full lg:w-1/2 p-4 lg:p-8 bg-[#f9f9f9] rounded-3xl shadow-sm hover:shadow-md transition-shadow duration-300"
+					className="w-full lg:w-1/2 p-4 lg:p-8 rounded-3xl hover:shadow-md transition-shadow duration-300"
 					initial={{ opacity: 0, y: 20 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.5, delay: 0.4 }}
